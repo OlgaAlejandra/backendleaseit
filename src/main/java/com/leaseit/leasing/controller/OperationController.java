e554224ed596b6272510a8ed4f62b1bd1512962f
Alejandra Ascue <olgaaa2003@gmail.com>
2022-11-21 10:13:14 -0500

    Opereciones

--- src/main/java/com/leaseit/leasing/controller/OperationController.java
+++ src/main/java/com/leaseit/leasing/controller/OperationController.java
@@ -1,10 +1,17 @@
 package com.leaseit.leasing.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.leaseit.leasing.entities.Operation;
 import com.leaseit.leasing.repository.ActivoRepository;
 import com.leaseit.leasing.repository.ArrendadorRepository;
 import com.leaseit.leasing.repository.OperacionRepository;
@@ -21,5 +28,10 @@ public class OperationController {
 	 @Autowired
 	 private ActivoRepository activoRepository;
 	 
+	 @GetMapping("operacion")
+	 public ResponseEntity<List<Operation>> getAllOperations(){
+		 List<Operation> operaciones = operationRepository.findAll();
+		 return new ResponseEntity<List<Operation>>(operaciones,HttpStatus.OK);
+	 }
 	 
 }
